cfa9b9bfdc2cdcfe6a2fae39cd13165472efd473
Steven Batcup <steve.batcup@gmail.com>
2018-12-04 19:51:51 +0000

    added basic auth

--- config/initializers/wp_connector.rb
+++ config/initializers/wp_connector.rb
@@ -1,8 +1,10 @@
 WpConnector.configure do |config|
 	if Rails.env == 'development'
 		config.wordpress_url = "http://admin.thearticle.jazz/wp-json/wp/v2"
+	  config.wordpress_basic_auth = { login: 'foo', password: 'bar' }
 	else
 	  config.wordpress_url = "https://staging.thearticle.com/wp-json/wp/v2"
+	  config.wordpress_basic_auth = { login: 'londonbridge', password: 'B37ys0m2w' }
 	end
   config.wp_connector_api_key = "Y9H3FFF2P91BX47"
   config.wp_api_paginated_models = %w(articles news_articles posts)
