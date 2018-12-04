f08c9aca76414c3485cda11a0435b60bbb51e196
Steven Batcup <steve.batcup@gmail.com>
2018-12-04 19:22:28 +0000

    oops

--- config/initializers/wp_connector.rb
+++ config/initializers/wp_connector.rb
@@ -2,9 +2,8 @@ WpConnector.configure do |config|
 	if Rails.env == 'development'
 		config.wordpress_url = "http://admin.thearticle.jazz/wp-json/wp/v2"
 	else
-	  config.wordpress_url = "http://staging.thearticle.com/wp-json/wp/v2"
+	  config.wordpress_url = "https://staging.thearticle.com/wp-json/wp/v2"
 	end
-	# puts "******** Env for WpConnector #{Rails.env} | #{config.wordpress_url} ********"
   config.wp_connector_api_key = "Y9H3FFF2P91BX47"
   config.wp_api_paginated_models = %w(articles news_articles posts)
 end
