1202bd9c57bfd63160d452ac2a16429e50624ef7
Steven Batcup <steve.batcup@gmail.com>
2018-12-13 14:53:01 +0000

    add title to reg form

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -4,6 +4,6 @@ class RegistrationsController < Devise::RegistrationsController
   protected
 
     def configure_permitted_parameters
-      devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :email, :password])
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:title, :first_name, :last_name, :email, :password])
     end
 end
