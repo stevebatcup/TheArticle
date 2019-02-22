b5cf2aa53482776aa8fe39490184f8bd709e1167
Steven Batcup <steve.batcup@gmail.com>
2019-02-22 11:37:31 +0000

    wp connector setup

--- config/initializers/wp_connector.rb
+++ config/initializers/wp_connector.rb
@@ -2,9 +2,11 @@ WpConnector.configure do |config|
 	if Rails.env == 'development'
 		config.wordpress_url = "http://admin.thearticle.jazz/wp-json/wp/v2"
 	  config.wordpress_basic_auth = { login: 'foo', password: 'bar' }
-	else
+	elsif Rails.env == 'staging'
 	  config.wordpress_url = "https://staging.thearticle.com/wp-json/wp/v2"
 	  config.wordpress_basic_auth = { login: 'londonbridge', password: 'B37ys0m2w' }
+	else
+	  config.wordpress_url = "http://www.thearticle.com/wp-json/wp/v2"
 	end
   config.wp_connector_api_key = "Y9H3FFF2P91BX47"
   config.wp_api_paginated_models = %w(articles news_articles posts)
