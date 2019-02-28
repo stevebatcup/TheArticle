7ea4b758a7429a0aa2911529836cfd95042c2397
Steven Batcup <steve.batcup@gmail.com>
2019-02-28 15:21:33 +0000

    bug fix

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -55,12 +55,14 @@ class ArticlesController < ApplicationController
 			end
 			@trending_articles = Article.trending.limit(Author.sponsors.any? ? 4 : 5).all.to_a
 			@trending_articles.insert(2, @sponsored_picks.first) if Author.sponsors.any?
-			@exchange_for_more = @article.exchanges.order(Arel.sql('RAND()')).first
-			@articles_in_same_exchange = @exchange_for_more.articles
-																										.includes(:exchanges).references(:exchanges)
-																										.includes(:author).references(:author)
-																										.where.not(id: @article.id)
-																										.limit(6)
+			@articles_in_same_exchange = []
+			if @exchange_for_more = @article.exchanges.order(Arel.sql('RAND()')).first
+				@articles_in_same_exchange = @exchange_for_more.articles
+																											.includes(:exchanges).references(:exchanges)
+																											.includes(:author).references(:author)
+																											.where.not(id: @article.id)
+																											.limit(6)
+			end
 		end
 		@article_share = {
 			'comments' => '',
