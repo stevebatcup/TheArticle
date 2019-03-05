cfbbdd204fc36269ebcd609bb4ea28751945eab6
Steven Batcup <steve.batcup@gmail.com>
2019-03-05 13:08:16 +0000

    photo rotation

--- app/uploaders/profile_photo_uploader.rb
+++ app/uploaders/profile_photo_uploader.rb
@@ -2,12 +2,13 @@ class ProfilePhotoUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
 
   storage :fog
-  process :auto_orient
+  # process :auto_orient
 
   def auto_orient
     manipulate! do |img|
-      img.tap(&:strip)
-      img.tap(&:auto_orient)
+      img = img.strip
+      img = img.auto_orient
+      img
     end
   end
 
@@ -20,8 +21,8 @@ class ProfilePhotoUploader < CarrierWave::Uploader::Base
   end
 
   version :square do
-    process :auto_orient
     process resize_to_fill: [150, 150]
+    process :auto_orient
 
     def store_dir
       "app/#{Rails.env}/users/profile-photo/square/#{model.id}"
