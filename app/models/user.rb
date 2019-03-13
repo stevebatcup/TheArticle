06029860756719d64551f4b27f26fab9df2e04e1
Steven Batcup <steve.batcup@gmail.com>
2019-03-13 12:49:00 +0000

    something

--- app/models/user.rb
+++ app/models/user.rb
@@ -84,10 +84,10 @@ class User < ApplicationRecord
     self.display_name = "#{first_name} #{last_name}"
     self.slug = self.username.downcase
 
-    self.notification_settings.build({ key: 'email_followers', value: 'as_it_happens' })
-    self.notification_settings.build({ key: 'email_exchanges', value: 'as_it_happens' })
-    self.notification_settings.build({ key: 'email_responses', value: 'daily' })
-    self.notification_settings.build({ key: 'email_replies', value: 'daily' })
+    self.notification_settings.build({ key: 'email_followers', value: 'never' })
+    self.notification_settings.build({ key: 'email_exchanges', value: 'never' })
+    self.notification_settings.build({ key: 'email_responses', value: 'never' })
+    self.notification_settings.build({ key: 'email_replies', value: 'never' })
 
     self.communication_preferences.build({ preference: 'newsletters_weekly', status: true })
     self.communication_preferences.build({ preference: 'newsletters_offers', status: true })
