efdf110b390856af0b732b5646817b9613b0e7b9
Steven Batcup <steve.batcup@gmail.com>
2019-04-01 16:07:59 +0100

    timezone scanning

--- app/controllers/wp_connector_controller.rb
+++ app/controllers/wp_connector_controller.rb
@@ -5,7 +5,7 @@ class WpConnectorController < ApplicationController
   def model_save
     model = params[:model].classify.constantize
     if ['post', 'article'].include?(params[:model].downcase)
-      render_json_200_or_404 model.schedule_create_or_update(wp_id_from_params, params[:publish_date])
+      render_json_200_or_404 model.schedule_create_or_update(wp_id_from_params, Time.parse("#{params[:publish_date]} #{Time.zone}"))
     else
       render_json_200_or_404 model.schedule_create_or_update(wp_id_from_params)
     end
