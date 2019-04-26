1f63b3a94c287299a26b02935b968134351bedda
Steven Batcup <steve.batcup@gmail.com>
2019-04-26 10:49:37 +0100

    fix

--- app/models/article.rb
+++ app/models/article.rb
@@ -359,7 +359,8 @@ class Article < ApplicationRecord
 					ad_page_id: ad_page_id,
 					ad_page_type: ad_page_type,
 					ad_publisher_id: ad_publisher_id,
-					ad_classes: 'unruly_video ads_box text-center'
+					ad_classes: 'unruly_video ads_box text-center',
+					ad_name: 'unruly'
 				},
 				{
 					position: 7,
@@ -367,7 +368,8 @@ class Article < ApplicationRecord
 					ad_page_id: ad_page_id,
 					ad_page_type: ad_page_type,
 					ad_publisher_id: ad_publisher_id,
-					ad_classes: 'ads_box text-center my-2'
+					ad_classes: 'ads_box text-center my-2',
+					ad_name: 'sidecolumn'
 				},
 				{
 					position: 11,
@@ -375,7 +377,8 @@ class Article < ApplicationRecord
 					ad_page_id: ad_page_id,
 					ad_page_type: ad_page_type,
 					ad_publisher_id: ad_publisher_id,
-					ad_classes: 'ads_box text-center my-2'
+					ad_classes: 'ads_box text-center my-2',
+					ad_name: 'sidecolumn'
 				}
 			]
 		else
@@ -386,7 +389,8 @@ class Article < ApplicationRecord
 					ad_page_id: ad_page_id,
 					ad_page_type: ad_page_type,
 					ad_publisher_id: ad_publisher_id,
-					ad_classes: 'unruly_video ads_box text-center'
+					ad_classes: 'unruly_video ads_box text-center',
+					ad_name: 'unruly'
 				}
 			]
 		end
