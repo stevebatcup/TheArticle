e530199198751f6ddd217f20fc1bb6ba083a509c
Steven Batcup <steve.batcup@gmail.com>
2019-04-30 12:24:32 +0100

    mailchimp unsubscribe

--- app/models/concerns/user/mailable.rb
+++ app/models/concerns/user/mailable.rb
@@ -13,6 +13,23 @@ module User::Mailable
   	preference && preference.status
   end
 
+  def set_opted_into_weekly_newsletters(status)
+    self.communication_preferences.find_by(preference: :newsletters_weekly).update_attribute(:status, status)
+  end
+
+  def set_opted_into_offers(status)
+    self.communication_preferences.find_by(preference: :newsletters_offers).update_attribute(:status, status)
+  end
+
+  def unsubscribe_all_emails
+    self.notification_settings.find_by(key: 'email_followers').update_attribute(:value, 'never')
+    self.notification_settings.find_by(key: 'email_exchanges').update_attribute(:value, 'never')
+    self.notification_settings.find_by(key: 'email_responses').update_attribute(:value, 'never')
+    self.notification_settings.find_by(key: 'email_replies').update_attribute(:value, 'never')
+    self.communication_preferences.find_by(preference: 'newsletters_weekly').update_attribute(:status, false)
+    self.communication_preferences.find_by(preference: 'newsletters_offers').update_attribute(:status, false)
+  end
+
   def send_weekly_categorisations_mail
     articles = []
     items = WeeklyUserMailItem.where(user_id: self.id, action_type: "categorisation").where("created_at >= DATE_SUB(CURDATE(), INTERVAL 1 WEEK)")
