7fd3a0fcecdde88b0231c10b0e27b755149b0b7b
Steven Batcup <steve.batcup@gmail.com>
2019-05-20 12:32:22 +0100

    dedupe the suggestions

--- app/models/concerns/user/suggestable.rb
+++ app/models/concerns/user/suggestable.rb
@@ -60,7 +60,11 @@ module User::Suggestable
       end
     end
 
-    # TODO: Same location
+    # de-dupe
+    existing_ids.uniq!
+    results.select! do |result|
+      existing_ids.include?(result[:user_id])
+    end
 
     save_suggestions results.shuffle.slice(0..limit)
   end
