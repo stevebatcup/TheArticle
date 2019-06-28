895adb7972ed6c8fb7d071b17a0c374887b5a3be
Steven Batcup <steve.batcup@gmail.com>
2019-06-28 12:20:17 +0100

    force ssl on staging

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -42,7 +42,7 @@ Rails.application.configure do
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 
-  config.force_ssl = false
+  config.force_ssl = true
 
   config.action_mailer.default_url_options = { host: 'railstaging.thearticle.com' }
 end
