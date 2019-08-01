f73b10ca4d07a3b71d18f79a26356d61b6a62355
Steven Batcup <steve.batcup@gmail.com>
2019-08-01 15:48:58 +0100

    followings order

--- app/controllers/user_followings_controller.rb
+++ app/controllers/user_followings_controller.rb
@@ -23,7 +23,7 @@ class UserFollowingsController < ApplicationController
 					if page == 1
 						@total = Follow.both_directions_for_user(user).size
 					end
-					@userFollowings = user.followings.order("created_at DESC").page(page).per(per_page).map(&:followed)
+					@userFollowings = user.followings.order("follows.created_at DESC").page(page).per(per_page).map(&:followed)
 					@userFollowers = user.followers.active.order("follows.created_at DESC").page(page).per(per_page)
 				end
 			end
