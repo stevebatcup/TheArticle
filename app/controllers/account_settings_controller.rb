9cf364409616819db26481afb7cba6e38d7c845e
Steven Batcup <steve.batcup@gmail.com>
2019-08-08 12:38:25 +0100

    acc settings sponsore pick

--- app/controllers/account_settings_controller.rb
+++ app/controllers/account_settings_controller.rb
@@ -4,7 +4,7 @@ class AccountSettingsController < ApplicationController
 	def edit
 		respond_to do |format|
 			format.html do
-				sponsored_picks = Author.get_sponsors_single_posts(nil, 3)
+				sponsored_picks = Author.get_sponsors_single_posts('sponsored-pick', 1, :random)
 				@trending_articles = Article.latest.limit(Author.sponsors.any? ? 4 : 5).all.to_a
 				@trending_articles.insert(2, sponsored_picks.first) if Author.sponsors.any?
 			end
