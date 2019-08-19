6acc2f5b639d9d0eb505b49bbb1b27e160cbb892
Steven Batcup <steve.batcup@gmail.com>
2019-08-19 12:31:02 +0100

    email me when blackliosting

--- app/models/black_list_user.rb
+++ app/models/black_list_user.rb
@@ -1,7 +1,7 @@
 class BlackListUser < ApplicationRecord
 	belongs_to	:user
 	belongs_to	:admin_user, class_name: 'User', foreign_key: :added_by_admin_user_id
-	# after_create	:email_developer
+	after_create	:email_developer
 
 	def admin_user_name
 		self.admin_user.display_name
