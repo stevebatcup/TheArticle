9f166f0b38c8110a149515d40379759f73f09b9e
Steven Batcup <steve.batcup@gmail.com>
2019-09-10 16:20:58 +0100

    autofollowing for daniel and olivia

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,24 +1,4 @@
-# Use this file to easily define all of your cron jobs.
-#
-# It's helpful, but not entirely necessary to understand cron before proceeding.
-# http://en.wikipedia.org/wiki/Cron
-
-# Example:
-#
-# set :output, "/path/to/my/cron_log.log"
-#
-# every 2.hours do
-#   command "/usr/bin/some_great_command"
-#   runner "MyModel.some_method"
-#   rake "some:great:rake:task"
-# end
-#
-# every 4.days do
-#   runner "AnotherModel.prune_old_records"
-# end
-
 # Learn more: http://github.com/javan/whenever
-
 every :reboot do
 	command "cd #{Dir.pwd} && RAILS_ENV=#{@environment} /home/ubuntu/.rbenv/bin/rbenv exec bundle exec rake ts:restart >> /var/www/thearticle/rails/shared/log/thinking_sphinx.log 2>&1"
 end
@@ -53,3 +33,11 @@ end
 every	1.day, at: '6:00 pm' do
 	rake "profiles:second_wizard_nudge >> /var/www/thearticle/rails/shared/log/notification_emails.log 2>&1"
 end
+
+every	1.day, at: '4:35 pm' do
+	rake "autofollow:olivia"
+end
+every	1.day, at: '4:30 pm' do
+	rake "autofollow:daniel"
+end
+
