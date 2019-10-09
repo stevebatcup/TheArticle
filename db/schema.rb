42f7b632e18c182cc5565617521800b517954dbf
Steven Batcup <steve.batcup@gmail.com>
2019-10-09 22:16:22 +0100

    add indexed to suggestions

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_04_115550) do
+ActiveRecord::Schema.define(version: 2019_10_09_211251) do
 
   create_table "account_deletions", options: "ENGINE=InnoDB DEFAULT CHARSET=latin1", force: :cascade do |t|
     t.integer "user_id"
@@ -430,6 +430,10 @@ ActiveRecord::Schema.define(version: 2019_10_04_115550) do
     t.integer "status"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["author_article_count"], name: "index_profile_suggestions_on_author_article_count"
+    t.index ["reason"], name: "index_profile_suggestions_on_reason"
+    t.index ["status"], name: "index_profile_suggestions_on_status"
+    t.index ["user_id"], name: "index_profile_suggestions_on_user_id"
   end
 
   create_table "push_tokens", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
