7b958ad5475086d7c9a60eb1451a41aad6802e8d
Steven Batcup <steve.batcup@gmail.com>
2020-01-08 16:28:44 +0000

    bug fix in tag query

--- app/models/article.rb
+++ app/models/article.rb
@@ -500,11 +500,12 @@ class Article < ApplicationRecord
 	def self.search_for_words(query, from_tag=false)
 		if from_tag
 			tag = KeywordTag.find_by(slug: query)
-			other_tags = KeywordTag.where.not(id: tag.id).where("LOWER(name) LIKE :query", query: "%#{sanitize_sql_like(query.downcase)}%")
+			other_tags = KeywordTag.where("LOWER(name) LIKE :query", query: "%#{sanitize_sql_like(query.downcase)}%")
+			other_tags = other_tags.where.not(id: tag.id) unless tag.nil?
 			other_tags.each do |ot|
 				query << " | #{ot.name}"
 			end
-			query = "#{query} | #{tag.name}"
+			query = "#{query} | #{tag.name}" unless tag.nil?
 		end
 		query = "#{query} | #{query.pluralize}" unless query == query.pluralize
 		articles = Article.search(query, order: 'published_at DESC', page: 1, per_page: 500).to_a
