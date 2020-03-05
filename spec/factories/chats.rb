8faaf3ec49b63b96ecaca92a676d635e04f94ce8
Steven Batcup <steve.batcup@gmail.com>
2020-03-05 17:17:20 +0000

    factories for chat specs

--- spec/factories/chats.rb
+++ spec/factories/chats.rb
@@ -11,4 +11,24 @@ FactoryGirl.define do
 		body "My ridiculous response to your silly old ice breaker"
 		association :user, factory: :initial_recipient, strategy: :build_stubbed
 	end
+
+	sequence :email do |n|
+		"article_user_#{n}@test.com"
+	end
+
+	factory :initial_sender, class: User do
+		first_name "Bob"
+		last_name "Smith"
+		email
+		password "123wer345123tyu"
+		confirmed_at 4.days.ago
+	end
+
+	factory :initial_recipient, class: User do
+		first_name "Jane"
+		last_name "Jones"
+		email
+		password "l2kj3k42k3j42mlsdjn"
+		confirmed_at 12.days.ago
+	end
 end
