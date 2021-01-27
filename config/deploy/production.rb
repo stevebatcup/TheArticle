418c5fdc96db5e94ec7cdebca59da6825487169b
Steven Batcup <steve.batcup@gmail.com>
2021-01-27 13:32:51 +0000

    upgraded to ruby 2.5.7

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -46,7 +46,7 @@ set :ssh_options, {
   keys: %w(/Users/steve/.ssh/thearticle_live.pem)
 }
 
-set :rbenv_ruby, '2.5.0'
+set :rbenv_ruby, '2.5.7'
 
 # The server-based syntax can be used to override options:
 # ------------------------------------
